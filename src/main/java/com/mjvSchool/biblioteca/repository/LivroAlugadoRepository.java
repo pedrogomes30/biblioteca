9513a88a9f2554a0271e126c827374a20e6890ca
NewarkX <brunowmanha@gmail.com>
2022-03-24 14:24:06 -0300

    desfazendo algumas coisas

--- src/main/java/com/mjvSchool/biblioteca/repository/LivroAlugadoRepository.java
+++
@@ -1,8 +0,0 @@
-package com.mjvSchool.biblioteca.repository;
-
-import com.mjvSchool.biblioteca.model.Livro;
-import com.mjvSchool.biblioteca.model.LivroAlugado;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface LivroAlugadoRepository extends JpaRepository<LivroAlugado,Integer> {
-}
--- src/main/java/com/mjvSchool/biblioteca/repository/PagamentoRepository.java
+++
@@ -1,8 +0,0 @@
-package com.mjvSchool.biblioteca.repository;
-
-import com.mjvSchool.biblioteca.model.Livro;
-import com.mjvSchool.biblioteca.model.Pagamentos;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface PagamentoRepository extends JpaRepository<Pagamentos,Integer> {
-}
